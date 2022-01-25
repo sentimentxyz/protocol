3286598081bee844f7905aa813ec00288052db3a
r0ohafza <coldroohafza@gmail.com>
2022-01-25 08:53:39 -0800

    test: add end to end tests for borrowing flow

---
+++ src/test/BorrowingFlow.t.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@ds-test/src/test.sol";
+
+import "./Cheatcode.sol";
+
+import "./Setup.sol";
+
+import "../interface/IAccount.sol";
+
+contract BorrowingFlowTest is Test {
+    
+    address user1 = cheatCode.addr(1);
+
+    function setUp() public {
+        basicSetup();
+        token.mint(user1, 1000);
+        
+        cheatCode.deal(user1, 1000);
+        cheatCode.startPrank(user1);
+        
+        token.approve(address(ltoken), type(uint).max);
+        ltoken.deposit(999);
+        
+        lEther.deposit{value: 999}();
+        cheatCode.stopPrank();
+    }
+
+    function testMarginAccountCreation() public {
+        address user = cheatCode.addr(2);
+        accountManager.openAccount(user);
+        address[] memory accounts = userRegistry.getMarginAccounts(user);
+        assertEq(IAccount(accounts[0]).ownerAddr(), user);
+    }
+
+    function testDepost() public {
+        address user = cheatCode.addr(2);
+        accountManager.openAccount(user);
+        
+        cheatCode.deal(user, 100);
+        token.mint(user, 100);
+        
+        address[] memory accounts = userRegistry.getMarginAccounts(user);
+        address marginAccount = accounts[0];
+        
+        cheatCode.startPrank(user);
+        
+        accountManager.depositEth{value: 10}(marginAccount);
+        assertEq(marginAccount.balance, 10);
+        assertEq(user.balance, 90);
+
+        token.approve(marginAccount, type(uint).max);
+        accountManager.deposit(marginAccount, address(token), 10);
+
+    }
+}
--- src/test/Setup.sol
+++ src/test/Setup.sol
@@ -15,6 +15,7 @@ import "../AccountManager.sol";
 import "../RiskEngine.sol";
 import "../UserRegistry.sol";
 import "../AccountFactory.sol";
+import "../Account.sol";
 
 contract Test is DSTest {
     CheatCode cheatCode = CheatCode(HEVM_ADDRESS);
@@ -29,21 +30,28 @@ contract Test is DSTest {
     RiskEngine public riskEngine;
     UserRegistry public userRegistry;
     AccountFactory public factory;
+    Account public marginAccount;
 
     function setUpLEther() public {
         lEther = new LEther("LEther", "LETH", 1, address(0), address(rateModel), address(accountManager), 1);
+        accountManager.setLTokenAddress(address(0), address(lEther));
+        accountManager.toggleCollateralState(address(0));
     }
 
     function setUpLtoken() public {
         token = new ERC20PresetMinterPauser("Sentiment", "STM");
         ltoken = new LERC20("LSentiment", "LSTM", 1, address(token), address(rateModel), address(accountManager), 1);
+        accountManager.setLTokenAddress(address(token), address(ltoken));
+        accountManager.toggleCollateralState(address(token));
     }
 
     function setUpAccountManager() public {
         riskEngine = setUpRiskEngine();
-        factory = new AccountFactory(address(0));
+        marginAccount = new Account();
+        factory = new AccountFactory(address(marginAccount));
         userRegistry = new UserRegistry();
         accountManager = new AccountManager(address(riskEngine), address(factory), address(userRegistry));
+        userRegistry.setAccountManagerAddress(address(accountManager));
     }
 
     function setUpRiskEngine() public returns (RiskEngine engine) {
