0ede556845252e343ce41f344a0cdd8ff736ea39
r0ohafza <coldroohafza@gmail.com>
2022-01-31 08:32:41 -0800

    refactor: move storageSlot to utils

---
+++ src/utils/Storage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+library StorageSlot {
+    function getAddressAt(bytes32 slot) internal view returns (address a) {
+        assembly {
+            a := sload(slot)
+        }
+    }
+
+    function setAddressAt(bytes32 slot, address address_) internal {
+        assembly {
+            sstore(slot, address_)
+        }
+    }
+}
