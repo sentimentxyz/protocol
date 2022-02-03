0c95f183b8a1c8e107f47eff656054cf6cd52038
r0ohafza <coldroohafza@gmail.com>
2022-02-03 09:32:08 -0800

    fix: rename pause to paused

--- src/utils/Pausable.sol
+++ src/utils/Pausable.sol
@@ -5,22 +5,22 @@ import "./Ownable.sol";
 import "./Errors.sol";
 
 abstract contract Pausable is Ownable {
-    bool public pause;
+    bool public paused;
 
     event PauseToggled(address indexed admin, bool pause);
 
     modifier whenNotPaused() {
-        if (pause) revert Errors.ContractPaused();
+        if (paused) revert Errors.ContractPaused();
         _;
     }
 
     modifier whenPaused() {
-        if (!pause) revert  Errors.ContractNotPaused();
+        if (!paused) revert  Errors.ContractNotPaused();
         _;
     }
 
-    function setPause(bool _pause) public adminOnly {
-        pause = _pause;
-        emit PauseToggled(msg.sender, pause);
+    function togglePause() public adminOnly {
+        paused = !paused;
+        emit PauseToggled(msg.sender, paused);
     }
 }
