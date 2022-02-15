2798b5994386342cd4b4a8a5702f9e7ff63f7533
Ruvaag <ruvaag@gmail.com>
2022-02-16 01:42:16 +0530

    refactor: implement a common getBorrowBalance function (#94)

--- src/core/tokens/LToken.sol
+++ src/core/tokens/LToken.sol
@@ -92,22 +92,46 @@ abstract contract LToken is Pausable, ILToken {
     }
 
     // Utility Functions
-    function updateState() external {
-        _updateState();
-    }
-    
-    // TODO Refactor to a single getBorrowBalance function that returns the debt + interest 
-    function currentBorrowBalance(address account) external returns (uint) {
-        if(_borrowBalance(account) == 0) return 0;
-        _updateState();
-        return _borrowBalance(account);
+    function updateState() external { _updateState(); }
+
+    function getBorrowBalance(address account) external view returns (uint) {
+        return (borrowBalanceFor[account].principal == 0) ? 0 :
+                borrowBalanceFor[account].principal
+                .mul((lastUpdated == block.number) ? borrowIndex : _getBorrowIndex(_getRateFactor()))
+                .div(borrowBalanceFor[account].interestIndex);
     }
 
-    function storedBorrowBalance(address account) external view returns (uint) {
-        return _borrowBalance(account);
+    // Internal Accounting Functions
+    /// @notice Amount of underlying assets currently held by this contract
+    function _getBalance() internal view virtual returns (uint);
+
+    // Rate Factor = Block Delta * Interest Rate Per Block
+    function _getRateFactor() internal view returns (uint) {
+        return (block.number - lastUpdated).fromUint()
+            .mul(IRateModel(rateModel).getBorrowRate(_getBalance(), totalBorrows, totalReserves));
     }
 
-    // Internal Functions
+    function _getBorrowIndex(uint rateFactor) internal view returns (uint) {
+        return borrowIndex.mul(1e18 + rateFactor);
+    }
+
+    // TODO Is there a way to update exchangeRate without checking for zero totalSupply?
+    function _updateState() internal {
+        if(lastUpdated == block.number) return;
+
+        uint rateFactor = _getRateFactor();
+        uint interestAccrued = totalBorrows.mul(rateFactor);
+
+        // Store results
+        borrowIndex = _getBorrowIndex(rateFactor);
+        totalBorrows += interestAccrued;
+        totalReserves += interestAccrued.mul(reserveFactor);
+        exchangeRate = (totalSupply == 0) ? exchangeRate :
+            (_getBalance() + totalBorrows - totalReserves).div(totalSupply);
+        lastUpdated = block.number;
+    }
+
+    // Internal ERC20 Functions
     function _mint(address to, uint256 value) internal {
         balanceOf[to] += value;
         totalSupply += value;
@@ -119,39 +143,6 @@ abstract contract LToken is Pausable, ILToken {
         totalSupply -= value;
         emit Transfer(from, address(0), value);
     }
-
-    function _borrowBalance(address account) internal view returns (uint) {
-        if(borrowBalanceFor[account].principal == 0) return 0;
-        return borrowBalanceFor[account].principal
-                .mul(_getBorrowIndex(_getRateFactor()))
-                .div(borrowBalanceFor[account].interestIndex);
-    }
-
-    function _updateState() internal {
-        if(lastUpdated == block.number) return;
-
-        uint rateFactor = _getRateFactor();
-        uint interestAccrued = totalBorrows.mul(rateFactor);
-
-        // Store results
-        borrowIndex = _getBorrowIndex(rateFactor);
-        totalBorrows = totalBorrows + interestAccrued;
-        totalReserves = interestAccrued.mul(reserveFactor) + totalReserves;
-        exchangeRate = (totalSupply == 0) ? exchangeRate :
-            (_getBalance() + totalBorrows - totalReserves).div(totalSupply);
-        lastUpdated = block.number;
-    }
-
-    function _getRateFactor() internal view returns (uint) {
-        return ((block.number - lastUpdated).fromUint())
-        .mul(IRateModel(rateModel).getBorrowRate(_getBalance(), totalBorrows, totalReserves));
-    }
-
-    function _getBorrowIndex(uint rateFactor) internal view returns (uint) {
-        return borrowIndex.mul(1e18 + rateFactor);
-    }
-
-    function _getBalance() internal view virtual returns (uint);
 
     // Admin-only functions
     function setAccountManager(address _accountManager) external adminOnly {
