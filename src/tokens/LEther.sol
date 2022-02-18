881b269b1ffbb404408ed16a5ef1a408fef949ff
ruvaag <ruvaag@gmail.com>
2022-02-18 16:21:48 +0530

    refactor common funcs using transferUnderlying

--- src/tokens/LEther.sol
+++ src/tokens/LEther.sol
@@ -2,9 +2,7 @@
 pragma solidity ^0.8.10;
 
 import {LToken} from "./LToken.sol";
-import {Errors} from "../utils/Errors.sol";
 import {Helpers} from "../utils/Helpers.sol";
-import {IRateModel} from "../interface/core/IRateModel.sol";
 import {PRBMathUD60x18} from "@prb-math/contracts/PRBMathUD60x18.sol";
 
 contract LEther is LToken {
@@ -26,44 +24,17 @@ contract LEther is LToken {
         _initialExchangeRate
     ) {}
 
-    // Lender Functions
+    /// @notice deposit ETH in exchange for LETH
     function deposit() external payable {
         _updateState();
         _mint(msg.sender, msg.value.div(exchangeRate));
-    }
-
-    /// @param value ltoken amount to be withdrawn
-    function withdraw(uint value) external {
-        _updateState();
-        msg.sender.safeTransferETH(value.mul(exchangeRate));
-        _burn(msg.sender, value);
-    }
-
-    // Account Manager Functions
-    function lendTo(address account, uint value) external accountManagerOnly returns (bool) {
-        if(block.number != lastUpdated) _updateState();
-        bool isFirstBorrow = (borrowBalanceFor[account].principal == 0);
-        (bool success, ) = account.call{value: value}("");
-        if(!success) revert Errors.ETHTransferFailure();
-        totalBorrows += value;
-        borrowBalanceFor[account].principal += value;
-        borrowBalanceFor[account].interestIndex = borrowIndex;
-        return isFirstBorrow;
-    }
-
-    function collectFrom(address account, uint value) external accountManagerOnly returns (bool) {
-        if(block.number != lastUpdated) _updateState();
-        totalBorrows -= value;
-        borrowBalanceFor[account].principal -= value;
-        borrowBalanceFor[account].interestIndex = borrowIndex;
-        return (borrowBalanceFor[account].principal == 0);
     }
 
     function _getBalance() internal view override returns (uint) {
         return address(this).balance;
     }
 
-    function _redeemUnderlying(address to, uint value) internal override {
+    function _transferUnderlying(address to, uint value) internal override {
         to.safeTransferETH(value);
     }
     
