c54790b33cffd2174633d86c25f976984b7eae2e
Rooh Afza <96720500+r0ohafza@users.noreply.github.com>
2022-03-08 08:13:22 -0800

    fix: updating tokensIn and tokensOut in exec flow (#118)

--- src/interface/periphery/IController.sol
+++ src/interface/periphery/IController.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.10;
 interface IController {
     function canCall(
         address target,
-        bytes4 sig,
         bytes calldata data
     ) external returns (bool, address[] memory, address[] memory);
 }
