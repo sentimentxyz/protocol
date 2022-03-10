16b5d8d33e3f9f7a51fc23c187658a40fc8e3960
r0ohafza <coldroohafza@gmail.com>
2022-03-10 15:55:31 -0800

    test: add more tests

--- src/test/integrations/Curve.t.sol
+++ src/test/integrations/Curve.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+import {Errors} from "../../utils/Errors.sol";
 import {IAccount} from "../../interface/core/IAccount.sol";
 import {IntegrationTestBase} from "./utils/IntegrationTestBase.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -22,7 +23,7 @@ contract curveIntegrationTest is IntegrationTestBase {
         account = openAccount(user);
     }
 
-    function testSwapWEthUSDT(uint8 _value) public {
+    function testExchangeWEthUSDT(uint8 _value) public {
         // Setup
         cheats.assume(_value != 0);
         uint256 value = uint256(_value) * 1 ether;
@@ -41,11 +42,12 @@ contract curveIntegrationTest is IntegrationTestBase {
             value
         );
         bytes memory data = abi.encodeWithSignature(
-            "exchange(uint256,uint256,uint256,uint256)",
+            "exchange(uint256,uint256,uint256,uint256,bool)",
             uint256(2),
             uint256(0),
             value,
-            minValue
+            minValue,
+            false
         );
 
         // Test
@@ -57,5 +59,47 @@ contract curveIntegrationTest is IntegrationTestBase {
         assertGe(IERC20(USDT).balanceOf(account), minValue);
         assertEq(IERC20(WETH).balanceOf(account), 0);
         assertEq(IAccount(account).assets(0), USDT);
+    }
+
+    function testExchangeEthUSDT(uint8 _value) public {
+        // Setup
+        cheats.assume(_value != 0);
+        uint256 value = uint256(_value) * 1 ether;
+        cheats.deal(user, value);
+        deposit(user, account, address(0), value);
+
+        // Encode Data
+        uint256 minValue = IStableSwapPool(curveEthSwap).get_dy(
+            uint256(2),
+            uint256(0),
+            value
+        );
+        bytes memory data = abi.encodeWithSignature(
+            "exchange(uint256,uint256,uint256,uint256,bool)",
+            uint256(2),
+            uint256(0),
+            value,
+            minValue,
+            true
+        );
+
+        // Test
+        cheats.prank(user);
+        accountManager.exec(account, curveEthSwap, value, data);
+
+        // Assert
+        assertGe(IERC20(USDT).balanceOf(account), minValue);
+        assertEq(account.balance, 0);
+        assertEq(IAccount(account).assets(0), USDT);
+    }
+
+    function testSwapSigError(uint8 value, bytes4 signature) public {
+        // Setup
+        bytes memory data = abi.encodeWithSelector(signature);
+
+        // Test
+        cheats.prank(user);
+        cheats.expectRevert(Errors.FunctionCallRestricted.selector);
+        accountManager.exec(account, curveEthSwap, value, data);
     }
 }
