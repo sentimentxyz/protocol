8c83c546025fd25625c7cd9caf96d2f64329acf9
ruvaag <ruvaag@gmail.com>
2022-03-16 23:56:29 +0530

    refactor CurveIntegrationTest

--- src/test/integrations/Curve.t.sol
+++ src/test/integrations/Curve.t.sol
@@ -5,6 +5,8 @@ import {Errors} from "../../utils/Errors.sol";
 import {IAccount} from "../../interface/core/IAccount.sol";
 import {IntegrationTestBase} from "./utils/IntegrationTestBase.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {CurveCryptoSwapController} 
+    from "@controller/src/curve/CurveCryptoSwapController.sol";
 
 interface IStableSwapPool {    
     function get_dy(uint256, uint256, uint256) external view returns (uint256);
@@ -12,9 +14,11 @@ interface IStableSwapPool {
 
 
 contract CurveIntegrationTest is IntegrationTestBase {
-
-    address user = cheats.addr(1);
     address account;
+    address user = cheats.addr(1);
+    address constant tricryptoPool = 0x960ea3e3C7FB317332d990873d354E18d7645590;
+
+    CurveCryptoSwapController curveController;
 
     function setUp() public {
         setupContracts();
@@ -23,37 +27,47 @@ contract CurveIntegrationTest is IntegrationTestBase {
         account = openAccount(user);
     }
 
-    function testExchangeWEthUSDT(uint8 _value) public {
+    function setupCurveController() private {
+        curveController = new CurveCryptoSwapController(controller);
+        controller.updateController(tricryptoPool, curveController);
+        controller.toggleSwapAllowance(USDT);
+    }
+
+    function testSwapWethUsdt(uint64 amt) public {
         // Setup
-        cheats.assume(_value != 0);
-        uint256 value = uint256(_value) * 1 ether;
-        cheats.deal(user, value);
-        deposit(user, account, address(0), value);
+        cheats.assume(amt > 1e8 gwei); // min exchange amt 0.1 eth
+        deposit(user, account, address(0), amt);
 
         // Wrap Eth
-        bytes memory wethdata = abi.encodeWithSignature("deposit()");
         cheats.prank(user);
-        accountManager.exec(account, WETH, value, wethdata);
-        
-        // Encode Data
-        uint256 minValue = IStableSwapPool(curveEthSwap).get_dy(
-            uint256(2),
-            uint256(0),
-            value
+        accountManager.exec(
+            account,
+            WETH,
+            amt,
+            abi.encodeWithSignature("deposit()")
         );
+
+        // Compute expected amt received after the swap
+        uint256 minValue = IStableSwapPool(tricryptoPool).get_dy(
+            uint256(2), // WETH
+            uint256(0), // USDT
+            amt
+        );
+
+        // Encode calldata
         bytes memory data = abi.encodeWithSignature(
             "exchange(uint256,uint256,uint256,uint256,bool)",
-            uint256(2),
-            uint256(0),
-            value,
+            uint256(2), // WETH
+            uint256(0), // USDT
+            amt,
             minValue,
             false
         );
 
         // Test
         cheats.startPrank(user);
-        accountManager.approve(account, WETH, curveEthSwap, value);
-        accountManager.exec(account, curveEthSwap, 0, data);
+        accountManager.approve(account, WETH, tricryptoPool, amt);
+        accountManager.exec(account, tricryptoPool, 0, data);
 
         // Assert
         assertGe(IERC20(USDT).balanceOf(account), minValue);
@@ -61,45 +75,45 @@ contract CurveIntegrationTest is IntegrationTestBase {
         assertEq(IAccount(account).assets(0), USDT);
     }
 
-    function testExchangeEthUSDT(uint8 _value) public {
+    function testSwapEthUsdt(uint64 amt) public {
         // Setup
-        cheats.assume(_value != 0);
-        uint256 value = uint256(_value) * 1 ether;
-        cheats.deal(user, value);
-        deposit(user, account, address(0), value);
+        cheats.assume(amt > 1e8 gwei); // min exchange amt 0.1 eth
+        deposit(user, account, address(0), amt);
 
-        // Encode Data
-        uint256 minValue = IStableSwapPool(curveEthSwap).get_dy(
-            uint256(2),
-            uint256(0),
-            value
+        // Compute expected amt received after the swap
+        uint256 minValue = IStableSwapPool(tricryptoPool).get_dy(
+            uint256(2), // WETH
+            uint256(0), // USDT
+            amt
         );
+
+        // Encode Calldata
         bytes memory data = abi.encodeWithSignature(
             "exchange(uint256,uint256,uint256,uint256,bool)",
-            uint256(2),
-            uint256(0),
-            value,
+            uint256(2), // WETH
+            uint256(0), // USDT
+            amt,
             minValue,
             true
         );
 
         // Test
         cheats.prank(user);
-        accountManager.exec(account, curveEthSwap, value, data);
+        accountManager.exec(account, tricryptoPool, amt, data);
 
         // Assert
-        assertGe(IERC20(USDT).balanceOf(account), minValue);
         assertEq(account.balance, 0);
         assertEq(IAccount(account).assets(0), USDT);
+        assertGe(IERC20(USDT).balanceOf(account), minValue);
     }
 
-    function testExchangeSigError(uint8 value, bytes4 signature) public {
+    function testSwapSigError(uint64 amt, bytes4 sig) public {
         // Setup
-        bytes memory data = abi.encodeWithSelector(signature);
+        bytes memory data = abi.encodeWithSelector(sig);
 
         // Test
         cheats.prank(user);
         cheats.expectRevert(Errors.FunctionCallRestricted.selector);
-        accountManager.exec(account, curveEthSwap, value, data);
+        accountManager.exec(account, tricryptoPool, amt, data);
     }
 }
--- src/test/integrations/utils/IntegrationTestBase.sol
+++ src/test/integrations/utils/IntegrationTestBase.sol
@@ -3,28 +3,18 @@ pragma solidity ^0.8.10;
 
 import {TestBase} from "../../utils/TestBase.sol";
 import {WETHController} from "@controller/src/weth/WETHController.sol";
-import {CurveCryptoSwapController} 
-    from "@controller/src/curve/CurveCryptoSwapController.sol";
 
 contract IntegrationTestBase is TestBase {
     
     // Controller Contracts
     WETHController public wEthController;
-    CurveCryptoSwapController public curveController;
 
     // Arbitrum Contracts
-    address WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
-    address curveEthSwap = 0x960ea3e3C7FB317332d990873d354E18d7645590;
-    address USDT = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+    address constant WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+    address constant USDT = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
 
     function setupWEthController() internal {
-        wEthController = new WETHController(WETH);
-        controller.updateController(WETH, wEthController);
-    }
-
-    function setupCurveController() internal {
-        curveController = new CurveCryptoSwapController(controller);
-        controller.updateController(curveEthSwap, curveController);
-        controller.toggleSwapAllowance(USDT);
+        wEthController = new WETHController(address(WETH));
+        controller.updateController(address(WETH), wEthController);
     }
 }
