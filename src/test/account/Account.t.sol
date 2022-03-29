3db476ea7ecda35adeca8227da2eb83833b140e4
r0ohafza <coldroohafza@gmail.com>
2022-03-28 17:27:52 -0700

    fix: testSweepTo failing for amt = 0

--- src/test/account/Account.t.sol
+++ src/test/account/Account.t.sol
@@ -100,6 +100,7 @@ contract AccountTest is TestBase {
 
     function testSweepTo(address user, uint96 amt) public {
         // Setup
+        cheats.assume(amt != 0);
         testAddAsset(address(erc20));
         erc20.mint(address(account), amt);
         cheats.deal(address(account), amt);
