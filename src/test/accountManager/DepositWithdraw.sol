e473abf7571f2949f2245aac8ad25778f7eb8798
Rooh Afza <96720500+r0ohafza@users.noreply.github.com>
2022-03-29 11:41:47 -0700

    fix: test pipeline (#132)
    * fix: collateralTypeRestricted error
    
    * fix: testSweepTo failing for amt = 0

--- src/test/accountManager/DepositWithdraw.sol
+++ src/test/accountManager/DepositWithdraw.sol
@@ -87,6 +87,7 @@ contract AccountManagerDepositWithdrawTest is TestBase {
         public
     {
         // Test
+        cheats.assume(token != address(0));
         cheats.prank(owner);
         cheats.expectRevert(Errors.CollateralTypeRestricted.selector);
         accountManager.deposit(account, token, value);
