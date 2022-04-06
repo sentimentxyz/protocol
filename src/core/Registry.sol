331f6c3c60f178d0c1de66d635a8e3cf84f36efd
ruvaag <ruvaag@gmail.com>
2022-04-06 11:21:23 +0530

    refactor setAddress and setLToken

--- src/core/Registry.sol
+++ src/core/Registry.sol
@@ -22,65 +22,29 @@ contract Registry is Ownable, IRegistry {
             revert Errors.AccountManagerOnly();
         _;
     }
-    
-    // Contract Registry Functions
 
     function setAddress(string calldata id, address _address) 
         external 
         adminOnly 
     {
-        if (_address == address(0)) revert Errors.ZeroAddress();
-        if (addressFor[id] == address(0)) keys.push(id);
+        if (addressFor[id] == address(0)) {
+            if (_address == address(0)) revert Errors.ZeroAddress();
+            keys.push(id);
+        }
+        else if (_address == address(0)) removeKey(id);
+
         addressFor[id] = _address;
     }
 
-    function removeAddress(string calldata id) external adminOnly {
-        uint len = keys.length;
-        bytes32 keyHash = keccak256(abi.encodePacked(id));
-        for(uint i; i < len; ++i) {
-            if (keyHash == keccak256(abi.encodePacked((keys[i])))) {
-                keys[i] = keys[len - 1];
-                keys.pop();
-                break;
-            }
-        }
-    }
-
-    // LToken Registry Functions
-
     function setLToken(address underlying, address lToken) external adminOnly {
-        if (LTokenFor[underlying] == address(0)) { // Add new LToken
-            require(lToken != address(0));
+        if (LTokenFor[underlying] == address(0)) {
+            if (lToken == address(0)) revert Errors.ZeroAddress();
             LTokenList.push(lToken);
-        } else if (lToken == address(0)) { // Remove existing LToken
-            removeFromLTokenList(LTokenFor[underlying]);
-        } else { // Update existing LToken
-            updateLTokenList(LTokenFor[underlying], lToken);
         }
+        else if (lToken == address(0)) removeLToken(LTokenFor[underlying]);
+        else updateLToken(LTokenFor[underlying], lToken);
+
         LTokenFor[underlying] = lToken;
-    }
-
-    // Array manipulation functions
-    function updateLTokenList(address lToken, address newLToken) internal {
-        uint len = LTokenList.length;
-        for(uint i; i < len; ++i) {
-            if(LTokenList[i] == lToken) {
-                LTokenList[i] = newLToken;
-                break;
-            }
-        }
-    }
-
-    function removeFromLTokenList(address token) internal {
-        uint len = LTokenList.length;
-        // Copy the last element in place of token and pop
-        for(uint i; i < len; ++i) {
-            if (LTokenList[i] == token) {
-                LTokenList[i] = LTokenList[len - 1];
-                LTokenList.pop();
-                break;
-            }
-        }
     }
 
     // User Registry Functions
@@ -130,6 +94,42 @@ contract Registry is Ownable, IRegistry {
             if (ownerFor[accounts[i]] == user) {
                 userAccounts[index] = accounts[i];
                 index++;
+            }
+        }
+    }
+
+    // Helper functions
+
+    function updateLToken(address lToken, address newLToken) internal {
+        uint len = LTokenList.length;
+        for(uint i; i < len; ++i) {
+            if(LTokenList[i] == lToken) {
+                LTokenList[i] = newLToken;
+                break;
+            }
+        }
+    }
+
+    function removeLToken(address underlying) internal {
+        uint len = LTokenList.length;
+        // Copy the last element in place of token and pop
+        for(uint i; i < len; ++i) {
+            if (underlying == LTokenList[i]) {
+                LTokenList[i] = LTokenList[len - 1];
+                LTokenList.pop();
+                break;
+            }
+        }
+    }
+
+    function removeKey(string calldata id) internal {
+        uint len = keys.length;
+        bytes32 keyHash = keccak256(abi.encodePacked(id));
+        for(uint i; i < len; ++i) {
+            if (keyHash == keccak256(abi.encodePacked((keys[i])))) {
+                keys[i] = keys[len - 1];
+                keys.pop();
+                break;
             }
         }
     }
