057d70bf858b5112ae191d38100dcb3815cc84d4
Ruvaag <ruvaag@gmail.com>
2022-04-09 05:33:34 +0530

    test: add tests for uni v2 swap and lps (#155)
    * add uni v2 swap tests
    
    * add tests for uni v2 Lp

---
+++ src/test/integrations/uniswap/UniV2Lp.t.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import {IERC20} from "../../../interface/tokens/IERC20.sol";
+import {IntegrationTestBase} from "../utils/IntegrationTestBase.sol";
+import {UniV2Controller} from "controller/uniswap/UniV2Controller.sol";
+
+contract UniV2LpIntegrationTest is IntegrationTestBase {
+    address account;
+    address user = cheats.addr(1);
+
+    address constant UNIV2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address constant WETH_USDT_LP = 0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852;
+    UniV2Controller uniV2Controller;
+    
+    function setupUniV2Controller() private {
+        uniV2Controller = new UniV2Controller(controller);
+        controller.updateController(UNIV2_ROUTER, uniV2Controller);
+        controller.toggleTokenAllowance(WETH);
+        controller.toggleTokenAllowance(WETH_USDT_LP);
+    }
+
+    function setUp() public {
+        setupContracts();
+        setupUniV2Controller();
+        setupWethController();
+        setupCurveController();
+
+        account = openAccount(user);
+    }
+
+    // All tests use the USDT/WETH Uni v2 pool
+
+    function testAddLiquidity(uint64 amt) public {
+        // WETH/USDT
+
+        // Setup
+        cheats.assume(amt > 1 ether);
+        deposit(user, account, address(0), uint(2) * amt);
+        wrapEth(account, amt, user);
+        swapEthUsdt(amt, account, user);
+        uint amtUsdt = IERC20(USDT).balanceOf(account);
+
+        // Encode calldata
+        bytes memory data = abi.encodeWithSignature(
+            "addLiquidity(address,address,uint256,uint256,uint256,uint256,address,uint256)",
+            WETH, USDT, amt, amtUsdt, 0, 0, account, 1893456000);
+        
+        // Test
+        cheats.startPrank(user);
+        accountManager.approve(account, WETH, UNIV2_ROUTER, type(uint).max);
+        accountManager.approve(account, USDT, UNIV2_ROUTER, type(uint).max);
+        accountManager.exec(account, UNIV2_ROUTER, 0, data);
+        cheats.stopPrank();
+
+        // Assert
+        assertGt(IERC20(WETH_USDT_LP).balanceOf(account), 0);
+    }
+
+    function testAddLiquidityEth(uint64 amt) public {
+        // ETH/USDT
+
+        // Setup
+        cheats.assume(amt > 1 ether);
+        deposit(user, account, address(0), uint(2) * amt);
+        swapEthUsdt(amt, account, user);
+        uint amtUsdt = IERC20(USDT).balanceOf(account);
+
+         // Encode calldata
+        bytes memory data = abi.encodeWithSignature(
+            "addLiquidityETH(address,uint256,uint256,uint256,address,uint256)",
+            USDT, amtUsdt, 0, 0, account, 1893456000);
+
+        // Test
+        cheats.startPrank(user);
+        accountManager.approve(account, USDT, UNIV2_ROUTER, type(uint).max);
+        accountManager.exec(account, UNIV2_ROUTER, amt, data);
+        cheats.stopPrank();
+
+        // Assert
+        assertGt(IERC20(WETH_USDT_LP).balanceOf(account), 0);
+    }
+
+    function testRemoveLiquidity(uint64 amt) public {
+        // Setup
+        cheats.assume(amt > 1 ether);
+        testAddLiquidity(amt);
+        uint lpTokens = IERC20(WETH_USDT_LP).balanceOf(account);
+
+        // Encode Calldata
+        bytes memory data = abi.encodeWithSignature(
+            "removeLiquidity(address,address,uint256,uint256,uint256,address,uint256)",
+            WETH, USDT, lpTokens, 0, 0, account, 1893456000);
+        
+        // Test
+        cheats.startPrank(user);
+        accountManager.approve(account, WETH_USDT_LP, UNIV2_ROUTER, type(uint).max);
+        accountManager.exec(account, UNIV2_ROUTER, 0, data);
+        cheats.stopPrank();
+
+        // Assert
+        assertGt(IERC20(WETH).balanceOf(account), 0);
+        assertGt(IERC20(USDT).balanceOf(account), 0);
+        assertEq(IERC20(WETH_USDT_LP).balanceOf(account), 0);
+    }
+
+    function testRemoveLiquidityEth(uint64 amt) public {
+        // Setup
+        cheats.assume(amt > 1 ether);
+        testAddLiquidity(amt);
+        uint lpTokens = IERC20(WETH_USDT_LP).balanceOf(account);
+
+        // Encode Calldata
+        bytes memory data = abi.encodeWithSignature(
+            "removeLiquidityETH(address,uint256,uint256,uint256,address,uint256)",
+            USDT, lpTokens, 0, 0, account, 1893456000);
+        
+        // Test
+        cheats.startPrank(user);
+        accountManager.approve(account, WETH_USDT_LP, UNIV2_ROUTER, type(uint).max);
+        accountManager.exec(account, UNIV2_ROUTER, 0, data);
+        cheats.stopPrank();
+
+        // Assert
+        assertGt(account.balance, 0);
+        assertEq(IERC20(WETH_USDT_LP).balanceOf(account), 0);
+        assertGt(IERC20(USDT).balanceOf(account), 0);
+    }
+
+}
---
+++ src/test/integrations/uniswap/UniV2Swap.t.sol
@@ -0,0 +1,181 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import {IERC20} from "../../../interface/tokens/IERC20.sol";
+import {IntegrationTestBase} from "../utils/IntegrationTestBase.sol";
+import {UniV2Controller} from "controller/uniswap/UniV2Controller.sol";
+
+contract UniV2SwapIntegrationTest is IntegrationTestBase {
+    address account;
+    address user = cheats.addr(1);
+
+    address constant UNIV2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    
+    // Path Arrays
+    address[] wethToUsdt = [WETH, USDT];
+    address[] usdtToWeth = [USDT, WETH];
+
+    UniV2Controller uniV2Controller;
+
+    function setupUniV2Controller() private {
+        uniV2Controller = new UniV2Controller(controller);
+        controller.updateController(UNIV2_ROUTER, uniV2Controller);
+        controller.toggleTokenAllowance(WETH);
+    }
+
+    function setUp() public {
+        setupContracts();
+        setupUniV2Controller();
+        setupWethController();
+        setupCurveController();
+        account = openAccount(user);
+    }
+
+    // All swaps are between ETH / USDT or WETH / USDT
+
+    function testSwapExactTokensForTokens(uint96 amt) public {
+        // WETH -> USDT
+
+        // Setup
+        cheats.assume(amt > 1 ether);
+        deposit(user, account, address(0), amt);
+        wrapEth(account, amt, user);
+
+        // Encode calldata
+        bytes memory data = abi.encodeWithSignature(
+            "swapExactTokensForTokens(uint256,uint256,address[],address,uint256)", 
+            amt, 0, wethToUsdt, account, 1893456000);
+        
+        // Test
+        cheats.startPrank(user);
+        accountManager.approve(account, WETH, UNIV2_ROUTER, type(uint).max);
+        accountManager.exec(account, UNIV2_ROUTER, 0, data);
+        cheats.stopPrank();
+
+        // Assert
+        assertEq(IERC20(WETH).balanceOf(account), 0);
+        assertGe(IERC20(USDT).balanceOf(account), 0);
+    }
+
+    function testSwapTokensForExactTokens(uint96 amt) public {
+        // WETH -> USDT
+        uint amountOut = 1000 * 1e6; // 1k USDC
+
+        // Setup
+        cheats.assume(amt > 1 ether);
+        deposit(user, account, address(0), amt);
+        wrapEth(account, amt, user);
+
+        // Encode calldata
+        bytes memory data = abi.encodeWithSignature(
+            "swapTokensForExactTokens(uint256,uint256,address[],address,uint256)", 
+            amountOut, amt, wethToUsdt, account, 1893456000);
+        
+        // Test
+        cheats.startPrank(user);
+        accountManager.approve(account, WETH, UNIV2_ROUTER, type(uint).max);
+        accountManager.exec(account, UNIV2_ROUTER, 0, data);
+        cheats.stopPrank();
+
+        // Assert
+        assertGe(IERC20(WETH).balanceOf(account), 0);
+        assertEq(IERC20(USDT).balanceOf(account), amountOut);
+    }
+
+    function testSwapExactEthForTokens(uint96 amt) public {
+        // ETH -> USDT
+
+        // Setup
+        cheats.assume(amt > 1 ether);
+        deposit(user, account, address(0), amt);
+
+        // Encode calldata
+        bytes memory data = abi.encodeWithSignature(
+            "swapExactETHForTokens(uint256,address[],address,uint256)", 
+            0, wethToUsdt, account, 1893456000);
+        
+        // Test
+        cheats.startPrank(user);
+        accountManager.approve(account, WETH, UNIV2_ROUTER, type(uint).max);
+        accountManager.exec(account, UNIV2_ROUTER, amt, data);
+        cheats.stopPrank();
+
+        // Assert
+        assertEq(account.balance, 0);
+        assertGt(IERC20(USDT).balanceOf(account), 0);
+    }
+
+    function testSwapEthForExactTokens(uint96 amt) public {
+        // ETH -> USDT
+        uint amountOut = 1000 * 1e6; // 1k USDC
+
+        // Setup
+        cheats.assume(amt > 1 ether);
+        deposit(user, account, address(0), amt);
+
+        // Encode calldata
+        bytes memory data = abi.encodeWithSignature(
+            "swapETHForExactTokens(uint256,address[],address,uint256)", 
+            amountOut, wethToUsdt, account, 1893456000);
+        
+        // Test
+        cheats.startPrank(user);
+        accountManager.approve(account, WETH, UNIV2_ROUTER, type(uint).max);
+        accountManager.exec(account, UNIV2_ROUTER, amt, data);
+        cheats.stopPrank();
+
+        // Assert
+        assertGe(account.balance, 0);
+        assertEq(IERC20(USDT).balanceOf(account), amountOut);
+    }
+
+    function testSwapExactTokensForEth(uint96 amt) public {
+        // USDT -> ETH
+
+        // Setup
+        cheats.assume(amt > 1 ether);
+        deposit(user, account, address(0), amt);
+        swapEthUsdt(amt, account, user);
+        uint amtUsdt = IERC20(USDT).balanceOf(account);
+
+        // Encode calldata
+        bytes memory data = abi.encodeWithSignature(
+            "swapExactTokensForETH(uint256,uint256,address[],address,uint256)", 
+            amtUsdt, 0, usdtToWeth, account, 1893456000);
+
+        // Test
+        cheats.startPrank(user);
+        accountManager.approve(account, USDT, UNIV2_ROUTER, type(uint).max);
+        accountManager.exec(account, UNIV2_ROUTER, 0, data);
+        cheats.stopPrank();
+
+        // Assert
+        assertGt(account.balance, 0);
+        assertEq(IERC20(USDT).balanceOf(account), 0);
+    }
+
+    function testSwapTokensForExactEth(uint96 amt) public {
+        // USDT -> ETH
+        uint amountOut = 1 ether;
+
+        // Setup
+        cheats.assume(amt > 15e9 gwei); // 1.5 ETH
+        deposit(user, account, address(0), amt);
+        swapEthUsdt(amt, account, user);
+        uint amtUsdt = IERC20(USDT).balanceOf(account);
+
+        // Encode calldata
+        bytes memory data = abi.encodeWithSignature(
+            "swapTokensForExactETH(uint256,uint256,address[],address,uint256)", 
+            amountOut, amtUsdt, usdtToWeth, account, 1893456000);
+        
+        // Test
+        cheats.startPrank(user);
+        accountManager.approve(account, USDT, UNIV2_ROUTER, type(uint).max);
+        accountManager.exec(account, UNIV2_ROUTER, 0, data);
+        cheats.stopPrank();
+
+        // Assert
+        assertGe(account.balance, amountOut);
+    }
+}
