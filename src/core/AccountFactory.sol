9840686b7456505efc4e3d917d0f12f79f25101f
Rooh Afza <96720500+r0ohafza@users.noreply.github.com>
2022-04-22 01:19:20 -0700

    doc: Account manager and factory natspec (#171)
    * doc: accountFactory natspec
    
    * doc: accountManager natspec

--- src/core/AccountFactory.sol
+++ src/core/AccountFactory.sol
@@ -5,16 +5,44 @@ import {BeaconProxy} from "../proxy/BeaconProxy.sol";
 import {IAccount} from "../interface/core/IAccount.sol";
 import {IAccountFactory} from "../interface/core/IAccountFactory.sol";
 
+/**
+    @title Account Factory
+    @notice Factory that creates Account as a beacon proxy
+*/
 contract AccountFactory is IAccountFactory {
+
+    /* -------------------------------------------------------------------------- */
+    /*                               STATE VARIABLES                              */
+    /* -------------------------------------------------------------------------- */
+
+    /// @notice Address of account beacon
     address public beacon;
 
+    /* -------------------------------------------------------------------------- */
+    /*                                 CONSTRUCTOR                                */
+    /* -------------------------------------------------------------------------- */
+
+    /**
+        @notice Contract constructor
+        @param _beacon address of account beacon
+    */
     constructor (address _beacon) {
         beacon = _beacon;
     }
 
+    /* -------------------------------------------------------------------------- */
+    /*                             EXTERNAL FUNCTIONS                             */
+    /* -------------------------------------------------------------------------- */
+
+    /**
+        @notice Account creator
+        @param accountManager Address of account manager
+        @dev Emits AccountCreated(account, accountManager) event
+        @return account Address of account created
+    */
     function create(address accountManager)
         external
-        returns (address account) 
+        returns (address account)
     {
         account = address(new BeaconProxy(beacon, accountManager));
         emit AccountCreated(account, accountManager);
