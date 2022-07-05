7cc42961347ef85742c03fb6a2055331180860df
Rooh Afza <96720500+r0ohafza@users.noreply.github.com>
2022-07-05 11:11:43 -0700

    test: add curve stable swap tests (#191)

--- src/test/integrations/utils/IntegrationTestBase.sol
+++ src/test/integrations/utils/IntegrationTestBase.sol
@@ -25,12 +25,14 @@ contract IntegrationTestBase is TestBase {
     // Ethereum Contracts
     address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address constant tricryptoPool = 0xD51a44d3FaE010294C616388b506AcdA1bfAAE46;
     address constant crv3crypto = 0xc4AD29ba4B3c580e6D59105FFf484999997675Ff;
 
     // Chainlink contracts
     address constant ETHUSD = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
     address constant USDTUSD = 0x3E7d1eAB13ad0104d2750B8863b489D65364e32D;
+    address constant DAIUSD = 0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9;
 
     function setupWethController() internal {
         wEthController = new WETHController(WETH);
@@ -53,7 +55,9 @@ contract IntegrationTestBase is TestBase {
     function setupChainLinkOracles() internal {
         chainlinkOracle = new ChainlinkOracle(AggregatorV3Interface(ETHUSD));
         oracle.setOracle(USDT, chainlinkOracle);
+        oracle.setOracle(DAI, chainlinkOracle);
         chainlinkOracle.setFeed(USDT, AggregatorV3Interface(USDTUSD));
+        chainlinkOracle.setFeed(DAI, AggregatorV3Interface(DAIUSD));
     }
 
     function setupOracles() internal {
