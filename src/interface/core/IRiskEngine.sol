ba1ac4d41449b3ae8e301fa38f8862d53a736890
r0ohafza <coldroohafza@gmail.com>
2022-07-07 15:53:46 -0700

    fix: add view modifier to IRiskEngine

--- src/interface/core/IRiskEngine.sol
+++ src/interface/core/IRiskEngine.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.10;
 
 interface IRiskEngine {
     function initDep() external;
-    function getBorrows(address account) external returns (uint);
+    function getBorrows(address account) external view returns (uint);
     function getBalance(address account) external view returns (uint);
-    function isAccountHealthy(address account) external returns (bool);
+    function isAccountHealthy(address account) external view returns (bool);
     function isBorrowAllowed(address account, address token, uint value)
-        external returns (bool);
+        external view returns (bool);
     function isWithdrawAllowed(address account, address token, uint value)
-        external returns (bool);
+        external view returns (bool);
 }
