be990a6a5bde40aab2016b286991f97a20903e46
r0ohafza <coldroohafza@gmail.com>
2022-10-28 13:50:27 -0700

    chore: add tests

--- src/test/integrations/BalancerArbi.t.sol
+++ src/test/integrations/BalancerArbi.t.sol
@@ -10,6 +10,9 @@ import {ComposableStableBalancerLPOracle} from "oracle/balancer/ComposableStable
 import {WeightedBalancerLPOracle} from "oracle/balancer/WeightedBalancerLPOracle.sol";
 import {IVault as IVaultOracle} from "oracle/balancer/IVault.sol";
 import {BalancerController} from "controller/balancer/BalancerController.sol";
+import {BalancerLPStakingController} from "controller/balancer/BalancerLPStakingController.sol";
+import {Stable2CurveGaugeOracle} from "oracle/curve/Stable2CurveGaugeOracle.sol";
+import {ZeroOracle} from "oracle/zero/ZeroOracle.sol";
 
 enum JoinKind { INIT, EXACT_TOKENS_IN_FOR_BPT_OUT, TOKEN_IN_FOR_EXACT_BPT_OUT }
 enum ExitKind {
@@ -24,25 +27,55 @@ contract BalancerArbiIntegrationTest is ArbiIntegrationTestBase {
     address account;
     address user = cheats.addr(1);
 
+    address constant LDO = 0x13Ad51ed4F1B7e9Dc168d8a00cB3f4dDD85EfA60;
+
     address balancerVault = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
-    address balancerStablePool = 0xFB5e6d0c1DfeD2BA000fBC040Ab8DF3615AC329c;
-    bytes32 balancerStablePoolID = 0xfb5e6d0c1dfed2ba000fbc040ab8df3615ac329c000000000000000000000159;
-    // address balancerWeightedPool = 0x96646936b91d6B9D7D0c47C496AfBF3D6ec7B6f8;
+    address BPTWBTCWETHUSDC = 0x64541216bAFFFEec8ea535BB71Fbc927831d0595;
+    bytes32 BPTWBTCWETHUSDCPoolID = 0x64541216bafffeec8ea535bb71fbc927831d0595000100000000000000000002;
+    address BPTWBTCWETHUSDCGauge = 0x104f1459a2fFEa528121759B238BB609034C2f01;
+
+    address BPTWSTETHWETH = 0xFB5e6d0c1DfeD2BA000fBC040Ab8DF3615AC329c;
+    bytes32 BPTWSTETHWETHPoolID = 0xfb5e6d0c1dfed2ba000fbc040ab8df3615ac329c000000000000000000000159;
+
+    address BPTWSTETHUSDC = 0x178E029173417b1F9C8bC16DCeC6f697bC323746;
+    bytes32 BPTWSTETHUSDCPoolID = 0x178e029173417b1f9c8bc16dcec6f697bc323746000200000000000000000158;
+    address BPTWSTETHUSDCGauge = 0x9232EE56ab3167e2d77E491fBa82baBf963cCaCE;
 
     BalancerController balancerController;
     ComposableStableBalancerLPOracle stableBalancerOracle;
-    // WeightedBalancerLPOracle weightedBalancerOracle;
+    WeightedBalancerLPOracle weightedBalancerOracle;
+    BalancerLPStakingController balancerStakingController;
+    Stable2CurveGaugeOracle balancerStakingOracle;
+    ZeroOracle zeroOracle;
 
     function setupBalancerController() internal {
+        accountManager.toggleCollateralStatus(USDC);
         balancerController = new BalancerController();
         controller.updateController(balancerVault, balancerController);
 
+        balancerStakingController = new BalancerLPStakingController();
+        controller.updateController(BPTWBTCWETHUSDCGauge, balancerStakingController);
+        controller.updateController(BPTWSTETHUSDCGauge, balancerStakingController);
+
         stableBalancerOracle = new ComposableStableBalancerLPOracle(oracle, IVaultOracle(balancerVault));
-        // weightedBalancerOracle = new WeightedBalancerLPOracle(oracle, IVaultOracle(balancerVault));
-        oracle.setOracle(balancerStablePool, stableBalancerOracle);
-        // oracle.setOracle(balancerWeightedPool, weightedBalancerOracle);
-        controller.toggleTokenAllowance(balancerStablePool);
-        // controller.toggleTokenAllowance(balancerWeightedPool);
+        oracle.setOracle(BPTWSTETHWETH, stableBalancerOracle);
+        controller.toggleTokenAllowance(BPTWSTETHWETH);
+
+        weightedBalancerOracle = new WeightedBalancerLPOracle(oracle, IVaultOracle(balancerVault));
+        oracle.setOracle(BPTWBTCWETHUSDC, weightedBalancerOracle);
+        oracle.setOracle(BPTWSTETHUSDC, weightedBalancerOracle);
+        controller.toggleTokenAllowance(BPTWBTCWETHUSDC);
+        controller.toggleTokenAllowance(BPTWSTETHUSDC);
+
+        balancerStakingOracle = new Stable2CurveGaugeOracle(oracle);
+        oracle.setOracle(BPTWSTETHUSDCGauge, balancerStakingOracle);
+        oracle.setOracle(BPTWBTCWETHUSDCGauge, balancerStakingOracle);
+        controller.toggleTokenAllowance(BPTWSTETHUSDCGauge);
+        controller.toggleTokenAllowance(BPTWBTCWETHUSDCGauge);
+
+        zeroOracle = new ZeroOracle();
+        oracle.setOracle(LDO, zeroOracle);
+        controller.toggleTokenAllowance(LDO);
     }
 
     function setUp() public {
@@ -51,10 +84,11 @@ contract BalancerArbiIntegrationTest is ArbiIntegrationTestBase {
         setupWethController();
         setupBalancerController();
         setupWSTETHOracle();
+        setupChainLinkOracles();
         account = openAccount(user);
     }
 
-    function testVaultJoinStablePool(uint64 amt) public {
+    function testVaultJoinBPTWBTCWETHUSDCPool(uint64 amt) public {
         // Setup
         cheats.assume(amt > 1e8 gwei);
         deal(WETH, user, amt, true);
@@ -63,9 +97,9 @@ contract BalancerArbiIntegrationTest is ArbiIntegrationTestBase {
         accountManager.deposit(account, WETH, amt);
 
         IAsset[] memory assets = new IAsset[](3);
-        assets[0] = IAsset(WSTETH);
+        assets[0] = IAsset(WBTC);
         assets[1] = IAsset(WETH);
-        assets[2] = IAsset(balancerStablePool);
+        assets[2] = IAsset(USDC);
 
         uint256[] memory amounts = new uint256[](3);
         amounts[0] = 0;
@@ -75,7 +109,7 @@ contract BalancerArbiIntegrationTest is ArbiIntegrationTestBase {
         // Encode calldata
         bytes memory data = abi.encodeWithSelector(
             0xb95cac28,
-            balancerStablePoolID,
+            BPTWBTCWETHUSDCPoolID,
             account,
             account,
             IVault.JoinPoolRequest(
@@ -92,165 +126,136 @@ contract BalancerArbiIntegrationTest is ArbiIntegrationTestBase {
 
         // Assert
         assertEq(IERC20(WETH).balanceOf(account), 0);
-        assertGt(IERC20(balancerStablePool).balanceOf(account), 0);
-        assertEq(IAccount(account).assets(0), balancerStablePool);
+        assertGt(IERC20(BPTWBTCWETHUSDC).balanceOf(account), 0);
+        assertEq(IAccount(account).assets(0), BPTWBTCWETHUSDC);
     }
 
-    // function testVaultExitStablePool() public {
-    //     // Setup
-    //     uint64 amt = 1e18;
-    //     testVaultJoinStablePool(amt);
+    function testBPTWBTCWETHUSDCStaking(uint64 amt) public {
+        testVaultJoinBPTWBTCWETHUSDCPool(amt);
 
-    //     IAsset[] memory assets = new IAsset[](3);
-    //     assets[0] = IAsset(DAI);
-    //     assets[1] = IAsset(USDC);
-    //     assets[2] = IAsset(USDT);
+        bytes memory data = abi.encodeWithSelector(
+            0xb6b55f25,
+            IERC20(BPTWBTCWETHUSDC).balanceOf(account)
+        );
 
-    //     uint256[] memory amounts = new uint256[](3);
-    //     amounts[0] = 0;
-    //     amounts[1] = 0;
-    //     amounts[2] = 1;
+        accountManager.approve(account, BPTWBTCWETHUSDC, BPTWBTCWETHUSDCGauge, type(uint).max);
+        accountManager.exec(account, BPTWBTCWETHUSDCGauge, 0, data);
 
-    //     // Encode calldata
-    //     bytes memory data = abi.encodeWithSelector(
-    //         0x8bdb3913,
-    //         0x06df3b2bbb68adc8b0e302443692037ed9f91b42000000000000000000000063,
-    //         account,
-    //         account,
-    //         IVault.ExitPoolRequest(
-    //             assets,
-    //             amounts,
-    //             abi.encode(ExitKind.EXACT_BPT_IN_FOR_ONE_TOKEN_OUT, IERC20(balancerStablePool).balanceOf(account), 2),
-    //             false
-    //         )
-    //     );
+        assertEq(IERC20(BPTWBTCWETHUSDC).balanceOf(account), 0);
+        assertGt(IERC20(BPTWBTCWETHUSDCGauge).balanceOf(account), 0);
+        assertEq(IAccount(account).assets(0), BPTWBTCWETHUSDCGauge);
+    }
 
-    //     // Test
-    //     cheats.startPrank(user);
-    //     accountManager.approve(account, balancerStablePool, balancerVault, type(uint).max);
-    //     accountManager.exec(account, balancerVault, 0, data);
-    //     cheats.stopPrank();
+    function testVaultJoinBPTWSTETHUSDCPool(uint24 amt) public {
+        // Setup
+        cheats.assume(amt > 1e6);
+        deal(USDC, user, amt, true);
+        startHoax(user);
+        IERC20(USDC).approve(address(accountManager), type(uint).max);
+        accountManager.deposit(account, USDC, amt);
 
-    //     // Assert
-    //     assertEq(IERC20(balancerStablePool).balanceOf(account), 0);
-    //     assertGt(IERC20(USDT).balanceOf(account), 0);
-    //     assertEq(IAccount(account).assets(0), USDT);
-    // }
+        IAsset[] memory assets = new IAsset[](2);
+        assets[0] = IAsset(WSTETH);
+        assets[1] = IAsset(USDC);
 
-    // function testVaultJoinWeightedPool(uint64 amt) public {
-    //     // Setup
-    //     cheats.assume(amt > 1e8 gwei);
-    //     deposit(user, account, address(0), amt);
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 0;
+        amounts[1] = amt;
 
-    //     IAsset[] memory assets = new IAsset[](2);
-    //     assets[0] = IAsset(USDC);
-    //     assets[1] = IAsset(address(0));
+        // Encode calldata
+        bytes memory data = abi.encodeWithSelector(
+            0xb95cac28,
+            BPTWSTETHUSDCPoolID,
+            account,
+            account,
+            IVault.JoinPoolRequest(
+                assets,
+                amounts,
+                abi.encode(JoinKind.EXACT_TOKENS_IN_FOR_BPT_OUT, amounts, 0),
+                false
+            )
+        );
 
-    //     uint256[] memory amounts = new uint256[](2);
-    //     amounts[0] = 0;
-    //     amounts[1] = amt;
+        // Test
+        accountManager.approve(account, USDC, balancerVault, type(uint).max);
+        accountManager.exec(account, balancerVault, 0, data);
 
-    //     // Encode calldata
-    //     bytes memory data = abi.encodeWithSelector(
-    //         0xb95cac28,
-    //         0x96646936b91d6b9d7d0c47c496afbf3d6ec7b6f8000200000000000000000019,
-    //         account,
-    //         account,
-    //         IVault.JoinPoolRequest(
-    //             assets,
-    //             amounts,
-    //             abi.encode(JoinKind.EXACT_TOKENS_IN_FOR_BPT_OUT, amounts, 0),
-    //             false
-    //         )
-    //     );
+        // Assert
+        assertEq(IERC20(USDC).balanceOf(account), 0);
+        assertGt(IERC20(BPTWSTETHUSDC).balanceOf(account), 0);
+        assertEq(IAccount(account).assets(0), BPTWSTETHUSDC);
+    }
 
-    //     // Test
-    //     cheats.startPrank(user);
-    //     accountManager.exec(account, balancerVault, amt, data);
-    //     cheats.stopPrank();
+    function testStakingBPTWSTETHUSDC(uint24 amt) public {
+        testVaultJoinBPTWSTETHUSDCPool(amt);
 
-    //     // Assert
-    //     assertEq(account.balance, 0);
-    //     assertGt(IERC20(balancerWeightedPool).balanceOf(account), 0);
-    //     assertEq(IAccount(account).assets(0), balancerWeightedPool);
-    // }
+        bytes memory data = abi.encodeWithSelector(
+            0xb6b55f25,
+            IERC20(BPTWSTETHUSDC).balanceOf(account)
+        );
 
-    // function testVaultExitWeightedPool(uint64 amt) public {
-    //     // Setup
-    //     testVaultJoinWeightedPool(amt);
+        accountManager.approve(account, BPTWSTETHUSDC, BPTWSTETHUSDCGauge, type(uint).max);
+        accountManager.exec(account, BPTWSTETHUSDCGauge, 0, data);
 
-    //     IAsset[] memory assets = new IAsset[](2);
-    //     assets[0] = IAsset(USDC);
-    //     assets[1] = IAsset(address(0));
+        assertEq(IERC20(BPTWSTETHUSDC).balanceOf(account), 0);
+        assertGt(IERC20(BPTWSTETHUSDCGauge).balanceOf(account), 0);
+        assertEq(IAccount(account).assets(0), BPTWSTETHUSDCGauge);
+    }
 
-    //     uint256[] memory amounts = new uint256[](2);
-    //     amounts[0] = 0;
-    //     amounts[1] = 1;
+    function testClaimStakingRewardsBPTWSTETHUSDC(uint24 amt) public {
+        testStakingBPTWSTETHUSDC(amt);
 
-    //     // Encode calldata
-    //     bytes memory data = abi.encodeWithSelector(
-    //         0x8bdb3913,
-    //         0x96646936b91d6b9d7d0c47c496afbf3d6ec7b6f8000200000000000000000019,
-    //         account,
-    //         account,
-    //         IVault.ExitPoolRequest(
-    //             assets,
-    //             amounts,
-    //             abi.encode(ExitKind.EXACT_BPT_IN_FOR_ONE_TOKEN_OUT, IERC20(balancerWeightedPool).balanceOf(account), 1),
-    //             false
-    //         )
-    //     );
+        bytes memory data = abi.encodeWithSelector(0xe6f1daf2);
 
-    //     // Test
-    //     cheats.startPrank(user);
-    //     accountManager.approve(account, balancerWeightedPool, balancerVault, type(uint).max);
-    //     accountManager.exec(account, balancerVault, 0, data);
-    //     cheats.stopPrank();
+        accountManager.exec(account, BPTWSTETHUSDCGauge, 0, data);
 
-    //     // Assert
-    //     assertEq(IERC20(balancerWeightedPool).balanceOf(account), 0);
-    //     assertGt(account.balance, 0);
-    //     assertEq(IAccount(account).getAssets().length, 1);
-    // }
+        assertEq(IERC20(BPTWSTETHUSDC).balanceOf(account), 0);
+        assertGt(IERC20(BPTWSTETHUSDCGauge).balanceOf(account), 0);
+        assertGt(IERC20(LDO).balanceOf(account), 0);
+        assertEq(IAccount(account).assets(0), BPTWSTETHUSDCGauge);
+        assertEq(IAccount(account).assets(1), BAL);
+        assertEq(IAccount(account).assets(2), LDO);
+    }
 
-    // function testSwapEthUSDC(uint64 amt) public {
-    //     // Setup
-    //     cheats.assume(amt > 1e8 gwei);
-    //     deposit(user, account, address(0), amt);
+    function testVaultJoinBPTWSTETHWETHPool(uint64 amt) public {
+        // Setup
+        cheats.assume(amt > 1e18);
+        deal(WETH, user, amt, true);
+        startHoax(user);
+        IERC20(WETH).approve(address(accountManager), type(uint).max);
+        accountManager.deposit(account, WETH, amt);
 
-    //     IVault.SingleSwap memory swap = IVault.SingleSwap(
-    //         0x96646936b91d6b9d7d0c47c496afbf3d6ec7b6f8000200000000000000000019,
-    //         uint8(SwapKind.GIVEN_IN),
-    //         IAsset(address(0)),
-    //         IAsset(USDC),
-    //         amt,
-    //         "0"
-    //     );
+        IAsset[] memory assets = new IAsset[](3);
+        assets[0] = IAsset(WSTETH);
+        assets[1] = IAsset(WETH);
+        assets[2] = IAsset(BPTWSTETHWETH);
 
-    //     IVault.FundManagement memory fund = IVault.FundManagement(
-    //         account,
-    //         false,
-    //         payable(account),
-    //         false
-    //     );
+        uint256[] memory amounts = new uint256[](3);
+        amounts[0] = 0;
+        amounts[1] = amt;
+        amounts[2] = 0;
 
-    //     // Encode calldata
-    //     bytes memory data = abi.encodeWithSelector(
-    //         0x52bbbe29,
-    //         swap,
-    //         fund,
-    //         1,
-    //         type(uint).max
-    //     );
+        // Encode calldata
+        bytes memory data = abi.encodeWithSelector(
+            0xb95cac28,
+            BPTWSTETHWETHPoolID,
+            account,
+            account,
+            IVault.JoinPoolRequest(
+                assets,
+                amounts,
+                abi.encode(JoinKind.EXACT_TOKENS_IN_FOR_BPT_OUT, amounts, 0),
+                false
+            )
+        );
 
-    //     // Test
-    //     cheats.startPrank(user);
-    //     accountManager.exec(account, balancerVault, amt, data);
-    //     cheats.stopPrank();
+        // Test
+        accountManager.approve(account, WETH, balancerVault, type(uint).max);
+        accountManager.exec(account, balancerVault, 0, data);
 
-    //     // Assert
-    //     assertEq(account.balance, 0);
-    //     assertGt(IERC20(USDC).balanceOf(account), 0);
-    //     assertEq(IAccount(account).assets(0), USDC);
-    // }
+        // Assert
+        assertEq(IERC20(WETH).balanceOf(account), 0);
+        assertGt(IERC20(BPTWBTCWETHUSDC).balanceOf(account), 0);
+        assertEq(IAccount(account).assets(0), BPTWBTCWETHUSDC);
+    }
 }
--- src/test/integrations/utils/ArbiIntegrationTestBase.sol
+++ src/test/integrations/utils/ArbiIntegrationTestBase.sol
@@ -6,6 +6,7 @@ import {WETHOracle} from "oracle/weth/WETHOracle.sol";
 import {WETHController} from "controller/weth/WETHController.sol";
 import {WSTETHOracle} from "oracle/wsteth/WSTETHOracle.sol";
 import {AggregatorV3Interface} from "oracle/chainlink/AggregatorV3Interface.sol";
+import {ChainlinkOracle} from "oracle/chainlink/ChainlinkOracle.sol";
 
 contract ArbiIntegrationTestBase is TestBase {
 
@@ -15,11 +16,21 @@ contract ArbiIntegrationTestBase is TestBase {
     // Oracle Contracts
     WETHOracle wethOracle;
     WSTETHOracle wstethOracle;
+    ChainlinkOracle chainlinkOracle;
 
     // Arbitrum Contracts
     address constant WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+    address constant WBTC = 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f;
     address constant USDT = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
     address constant WSTETH = 0x5979D7b546E38E414F7E9822514be443A4800529;
+    address constant USDC = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
+    address constant BAL = 0x040d1EdC9569d4Bab2D15287Dc5A4F10F56a56B8;
+
+    address constant BALUSD = 0xBE5eA816870D11239c543F84b71439511D70B94f;
+    address constant ETHUSD = 0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612;
+    address constant USDCUSD = 0x50834F3163758fcC1Df9973b6e91f0F0F0434aD3;
+    address constant USDTUSD = 0x3f3f5dF88dC9F13eac63DF89EC16ef6e7E25DdE7;
+    address constant WBTCUSD = 0x6ce185860a4963106506C203335A2910413708e9;
 
     function setupWethController() internal {
         wEthController = new WETHController(WETH);
@@ -35,7 +46,7 @@ contract ArbiIntegrationTestBase is TestBase {
     function setupWSTETHOracle() internal {
         wstethOracle = new WSTETHOracle(
             AggregatorV3Interface(0xB1552C5e96B312d0Bf8b554186F846C40614a540),
-            AggregatorV3Interface(0xCfE54B5cD566aB89272946F602D76Ea879CAb4a8),
+            AggregatorV3Interface(0x07C5b924399cc23c24a95c8743DE4006a32b7f2a),
             AggregatorV3Interface(0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612)
         );
         oracle.setOracle(WSTETH, wstethOracle);
@@ -50,5 +61,21 @@ contract ArbiIntegrationTestBase is TestBase {
 
         cheats.prank(owner);
         accountManager.exec(account, WETH, amt, data);
+    }
+
+    function setupChainLinkOracles() internal {
+        chainlinkOracle = new ChainlinkOracle(AggregatorV3Interface(ETHUSD));
+        oracle.setOracle(USDT, chainlinkOracle);
+        oracle.setOracle(USDC, chainlinkOracle);
+        oracle.setOracle(WBTC, chainlinkOracle);
+        oracle.setOracle(BAL, chainlinkOracle);
+        controller.toggleTokenAllowance(USDT);
+        controller.toggleTokenAllowance(BAL);
+        controller.toggleTokenAllowance(USDC);
+        controller.toggleTokenAllowance(WBTC);
+        chainlinkOracle.setFeed(USDT, AggregatorV3Interface(USDTUSD), 86400);
+        chainlinkOracle.setFeed(USDC, AggregatorV3Interface(USDCUSD), 86400);
+        chainlinkOracle.setFeed(WBTC, AggregatorV3Interface(WBTCUSD), 3600);
+        chainlinkOracle.setFeed(BAL, AggregatorV3Interface(BALUSD), 3600);
     }
 }
